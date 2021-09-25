20f81e1b536c640cbadb73d0c3489f1acbb18a7a
Deryck41 <77533149+Deryck41@users.noreply.github.com>
2021-09-25 13:32:37 +0300

    Task 2 complete

---
+++ Task-2/Task-2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputData = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("input data: " + inputData);
+            Console.ReadKey();
+        }
+    }
+}

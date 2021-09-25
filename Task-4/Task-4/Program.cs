a1acdd1b89ef2fc5ac561b69e1befa46add15b04
Deryck41 <77533149+Deryck41@users.noreply.github.com>
2021-09-25 13:57:21 +0300

    Task 4 complete

---
+++ Task-4/Task-4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse).Max());
+            // This gets the data, then translates to an array
+            // Then translates to an array of int and finds the max number
+
+            Console.ReadKey();
+        }
+    }
+}

45b08ab92ba77e569c8167e71e304d789e5164fd
Deryck41 <77533149+Deryck41@users.noreply.github.com>
2021-09-26 19:41:14 +0300

    Task 2.5 complete

---
+++ Task2-5/Task2-5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Task2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the lengths of the sides of the triangle.\n\nSide 1: ");
+            bool firstSuccessInput = float.TryParse(Console.ReadLine(), out float firstSide);
+            Console.Write("Side 2: ");
+            bool secondSuccessInput = float.TryParse(Console.ReadLine(), out float secondSide);
+            Console.Write("Side 3: ");
+            bool thirdSuccessInput = float.TryParse(Console.ReadLine(), out float thirdSide);
+
+            if (firstSuccessInput && secondSuccessInput && thirdSuccessInput)
+            {
+                if (firstSide + secondSide > thirdSide 
+                    && firstSide + thirdSide > secondSide
+                    && secondSide + thirdSide > firstSide)
+                {
+                    Console.WriteLine("\nThis triangle exists");
+                    if (firstSide == secondSide && secondSide == thirdSide)
+                    {
+                        Console.WriteLine("This triangle is equilateral");
+                    }
+                    else if(firstSide == secondSide
+                        || firstSide == thirdSide
+                        || secondSide == thirdSide)
+                    {
+                        Console.WriteLine("This triangle is isosceles");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nThis triangle does not exist");
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid input!");
+                Console.ResetColor();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

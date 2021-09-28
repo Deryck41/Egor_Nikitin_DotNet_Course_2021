6bf071b49c656377824fd99a802709edc35e4a37
Deryck41 <77533149+Deryck41@users.noreply.github.com>
2021-09-28 19:05:33 +0300

    The code is formatted

--- Task2-2/Task2-2/Program.cs
+++ Task2-2/Task2-2/Program.cs
@@ -6,38 +6,48 @@ namespace Task2_2
     {
         static void Main(string[] args)
         {
-            int firstNumber, secondNumber, thirdNumber;
-
             Console.Write("Enter three numbers through the whitelines.\n\nNumber 1: ");
-            bool firstSuccessInput = int.TryParse(Console.ReadLine(), out firstNumber);
+
+            bool firstSuccessInput = int.TryParse(Console.ReadLine(), out int firstNumber);
+
             Console.Write("Number 2: ");
-            bool secondSuccessInput = int.TryParse(Console.ReadLine(), out secondNumber);
+
+            bool secondSuccessInput = int.TryParse(Console.ReadLine(), out int secondNumber);
+
             Console.Write("Number 3: ");
-            bool thirdSuccessInput = int.TryParse(Console.ReadLine(), out thirdNumber);
+
+            bool thirdSuccessInput = int.TryParse(Console.ReadLine(), out int thirdNumber);
 
             if(firstSuccessInput && secondSuccessInput && thirdSuccessInput)
             {
                 int min = Math.Min(firstNumber, Math.Min(secondNumber, thirdNumber));
+
                 int max = Math.Max(firstNumber, Math.Max(secondNumber, thirdNumber));
+
                 int average;
+
                 if (min == firstNumber && max == secondNumber || max == firstNumber && min == secondNumber)
                 {
                     average = thirdNumber;
                 }
+
                 else if (min == firstNumber && max == thirdNumber || min == thirdNumber && max == firstNumber)
                 {
                     average = secondNumber;
                 }
+
                 else
                 {
                     average = thirdNumber;
                 }
+
                 Console.WriteLine($"\nIn ascending order: \t{min}\t{average}\t{max}");
                 Console.WriteLine($"In descending order: \t{max}\t{average}\t{min}");
                 Console.WriteLine("\nMin number: \t\t" + min);
                 Console.WriteLine("Average number: \t" + average);
                 Console.WriteLine("Max number: \t\t" + max);
             }
+
             else
             {
                 Console.ForegroundColor = ConsoleColor.Red;

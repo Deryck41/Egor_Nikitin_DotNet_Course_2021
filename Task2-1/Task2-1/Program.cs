444324a10074db219ddd5dbaf14b51d7c51ca948
Deryck41 <77533149+Deryck41@users.noreply.github.com>
2021-09-28 17:27:38 +0300

    The code is formatted

--- Task2-1/Task2-1/Program.cs
+++ Task2-1/Task2-1/Program.cs
@@ -4,24 +4,23 @@ namespace Task2_1
 {
     class Program
     {
-        
         static void Main(string[] args)
         {
             Console.Write("Input year: ");
-            int year;
-            bool successInput = int.TryParse(Console.ReadLine(), out year);
 
-            if (successInput)
+            if (int.TryParse(Console.ReadLine(), out int year))
             {
                 if (year % 4 == 0)
                 {
                     Console.WriteLine("This year is leap");
                 }
+
                 else
                 {
                     Console.WriteLine("This year is not leap");
                 }
             }
+
             else
             { 
                 Console.ForegroundColor = ConsoleColor.Red;

4f0e12bb0763fe5b97a0a93da0e047e0da7d121e
Deryck41 <77533149+Deryck41@users.noreply.github.com>
2021-10-08 23:49:09 +0300

    Task 3.2 complete

---
+++ Task3-2/Task3-2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Task3_2
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.Write("Input size of array: ");
+            if (!int.TryParse(Console.ReadLine(), out int size))
+            {
+                Console.WriteLine("Invalid input!");
+                return 0;
+            }
+
+
+            int[] numArray = new int[size];
+
+            Console.WriteLine("Input array: ");
+            for (int i = 0; i < size; i++)
+            {
+                if (!int.TryParse(Console.ReadLine(), out numArray[i]))
+                {
+                    Console.WriteLine("Invalid input!");
+                    return 0;
+                }
+            }
+
+            int sum = 0;
+            int count = 0;
+            foreach (int num in numArray)
+            {
+                if (num > 0)
+                {
+                    count++;
+                    sum += num;
+                }
+            }
+            Console.WriteLine("average: " + sum / count);
+
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}

99dd51a966e99ced06337644ddf549034135d8b9
Deryck41 <77533149+Deryck41@users.noreply.github.com>
2021-10-24 23:53:28 +0300

    Task 3.5 complete

---
+++ Task3-5/Task3-5/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Task3_5
+{
+    class Program
+    {
+        static void PrintArray(int[] array)
+        {
+            Console.Write("array: |");
+
+            foreach (int element in array)
+            {
+                Console.Write(element+"|");
+            }
+
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int size = random.Next(2, 20);
+            int[] numbers = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                numbers[i] = random.Next(-100, 100);
+            }
+
+            Console.WriteLine("Before sorting:");
+            PrintArray(numbers);
+
+            int buffer;
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                for (int j = i + 1; j < size; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        buffer = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = buffer;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nAfter sorting:");
+            PrintArray(numbers);
+            Console.ReadKey();
+        }
+    }
+}

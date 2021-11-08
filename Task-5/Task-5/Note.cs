2a2b3f28bf2840711f113504c53e75642c6e716f
Deryck41 <77533149+Deryck41@users.noreply.github.com>
2021-11-08 17:08:12 +0300

    Task 5 complete

---
+++ Task-5/Task-5/Note.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Note
+{
+    public int id { get; private set; }
+    public string title { get; private set; }
+    public string content { get; private set; }
+
+    public Note(int newId, string newTitle, string NewContent)
+    {
+        id = newId;
+        title = newTitle;
+        content = NewContent;
+    }
+
+    public void ChangeContent(string newContent) => content = newContent;
+
+    public void ChangeTitle(string newTitle) => title = newTitle;
+
+    public void AppendContent(string contentToAppend) => content += contentToAppend;
+}
---
+++ Task-5/Task-5/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Task_5
+{
+    
+    class Program
+    {
+
+        static void NoteMenu(int index, ref List<Note> notes)
+        {
+            bool localMenu = true;
+
+            while (localMenu)
+            {
+                Console.Write(">>>>> ");
+                string command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "!back":
+                        localMenu = false;
+                        break;
+                    case "!view":
+                        Manager.PrintNote(notes[index]);
+                        break;
+                    case "!edittitle":
+                        Console.WriteLine("Enter new title: ");
+                        notes[index].ChangeTitle(Console.ReadLine());
+                        break;
+                    case "!edittext":
+                        Console.WriteLine("Enter new content: ");
+                        notes[index].ChangeContent(Manager.MultilineInput());
+                        break;
+                    case "!appendtext":
+                        Console.WriteLine("Enter content to append: ");
+                        notes[index].AppendContent(Manager.MultilineInput());
+                        break;
+                }
+            }
+        }
+   
+        static void Main(string[] args)
+        {
+            int currentId = 0;
+            var notes = new List<Note>();
+            bool menu = true;
+
+            while (menu)
+            {
+                Console.Write(">>> ");
+                string command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "!q":
+                        menu = false;
+                        break;
+                    case "!list":
+                        Manager.PrintListOfNotes(notes);
+                        break;
+                    case "!add":
+                        notes.Add(Manager.GetNoteObject(currentId++));
+                        break;
+                    case "!help":
+                        Manager.PrintHelp();
+                        break;
+                    case "!goto":
+                        bool sucessIndexInput = int.TryParse(Console.ReadLine(), out int index);
+
+                        if (sucessIndexInput && index >= 0 && index < notes.Count)
+                        {
+                            NoteMenu(index, ref notes);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Input existing note id!");
+                        }
+                        break;
+                        
+                }
+            }
+        }
+    }
+}
